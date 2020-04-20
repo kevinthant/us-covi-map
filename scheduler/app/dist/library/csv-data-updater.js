95ef23d3d06274ddd5851071951fda5b81952cee
Kevin Thant <kevinthant@landzin.com>
2020-04-20 15:18:03 -0400

    Updating the covimap data update processing by optimizing the memory usage with line by line processing and only inserting the latest data that are not yet in the database

---
+++ scheduler/app/dist/library/csv-data-updater.js
@@ -0,0 +1,75 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+const csv = require('csv-parser');
+
+const downloader = require('receive-file');
+
+const fs = require('fs');
+
+const download = (url, filename) => {
+  const options = {
+    directory: "/tmp",
+    filename: filename,
+    timeout: 100000
+  };
+  return downloader(url, options);
+};
+
+const readCsvRow = async (url, rowUpdateCallback, onFinishCallback) => {
+  const filename = url.split('/').pop();
+
+  try {
+    await download(url, filename);
+    const readStream = fs.createReadStream(`/tmp/${filename}`).pipe(csv());
+
+    for await (const row of readStream) {
+      await rowUpdateCallback(row);
+    }
+
+    onFinishCallback();
+  } catch (err) {
+    console.log("Error in getting data: ", err);
+  }
+};
+
+const getLatestDateById = async (conn, query) => {
+  const [rows, fields] = await conn.promise().query(query);
+  const latestDateById = {};
+  rows.forEach(row => {
+    latestDateById[row.id] = new Date(row.date).getTime();
+  });
+  return latestDateById;
+};
+
+const csvDataUpdater = async ({
+  type,
+  conn,
+  csvUrl,
+  latestDataQuery,
+  schema,
+  getId,
+  updateQuery,
+  getUpdateParams
+}) => {
+  const latestDateById = await getLatestDateById(conn, latestDataQuery);
+  await conn.promise().execute(schema);
+  let count = 0;
+  await readCsvRow(csvUrl, async item => {
+    if (latestDateById[getId(item)] >= new Date(item.date).getTime()) {
+      return Promise.resolve(true);
+    }
+
+    count++;
+    await conn.promise().execute(updateQuery, getUpdateParams(item));
+  }, () => {
+    console.log(`Updated ${type} data, total = ${count}`);
+  });
+};
+
+var _default = csvDataUpdater;
+exports.default = _default;
--- scheduler/app/dist/scheduler/jobs/covimap-data-update.js
+++ scheduler/app/dist/scheduler/jobs/covimap-data-update.js
@@ -5,13 +5,56 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
+var _csvDataUpdater = _interopRequireDefault(require("./../../library/csv-data-updater"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 const mysql = require('mysql2');
 
 const config = require('config');
 
-const rp = require('request-promise');
+const updateStateData = async conn => {
+  const csvUrl = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv';
+  await (0, _csvDataUpdater.default)({
+    type: 'state',
+    conn,
+    csvUrl,
+    latestDataQuery: `select state as id, MAX(\`date\`) as date from state_cases  GROUP BY state`,
+    schema: `CREATE TABLE IF NOT EXISTS \`state_cases\` (
+      \`date\` date DEFAULT NULL,
+      \`state\` varchar(50) DEFAULT NULL,
+      \`fips\` varchar(20) DEFAULT NULL,
+      \`cases\` int(11) DEFAULT NULL,
+      \`deaths\` int(11) DEFAULT NULL
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;`,
+    getId: item => item.state,
+    updateQuery: 'INSERT INTO `state_cases` (date, state, fips, cases, deaths) VALUES (?, ?, ?, ?, ?)',
+    getUpdateParams: item => [item.date, item.state, item.fips, item.cases, item.deaths]
+  });
+};
 
-const csv = require('csvtojson');
+const updateCountyData = async conn => {
+  const csvUrl = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv';
+  await (0, _csvDataUpdater.default)({
+    type: 'county',
+    conn,
+    csvUrl,
+    latestDataQuery: `select CONCAT(county, state) as id, MAX(\`date\`) 
+    as date from county_cases GROUP BY county, state`,
+    schema: `CREATE TABLE IF NOT EXISTS \`county_cases\` (
+      \`date\` date DEFAULT NULL,
+      \`county\` varchar(50) DEFAULT NULL,
+      \`state\` varchar(50) DEFAULT NULL,
+      \`fips\` varchar(20) DEFAULT NULL,
+      \`cases\` int(11) DEFAULT NULL,
+      \`deaths\` int(11) DEFAULT NULL,
+      KEY \`county_state_index\` (\`county\`,\`state\`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;`,
+    getId: item => `${item.county}${item.state}`,
+    updateQuery: 'INSERT INTO `county_cases` (date, county, state, fips, cases, deaths) VALUES (?, ?, ?, ?, ?, ?)',
+    getUpdateParams: item => [item.date, item.county, item.state, item.fips, item.cases, item.deaths]
+  });
+};
 
 const {
   host,
@@ -20,20 +63,8 @@ const {
   database
 } = config.get('db');
 
-const getData = url => {
-  return new Promise((resolve, reject) => {
-    rp(url).then(str => {
-      csv().fromString(str).then(resolve).catch(reject);
-    }).catch(reject);
-  });
-};
-
 const CovimapDataUpdate = async jobData => {
   console.log(`\n[${new Date()}]Running CoviMapDataUpdate`, jobData);
-  const countyDataUrl = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv';
-  const stateDataUrl = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv';
-  const stateData = await getData(stateDataUrl);
-  const countyData = await getData(countyDataUrl);
   const conn = mysql.createConnection({
     host,
     user: username,
@@ -42,37 +73,9 @@ const CovimapDataUpdate = async jobData => {
   });
 
   try {
-    await conn.promise().execute(`CREATE TABLE IF NOT EXISTS \`state_cases\` (
-        \`date\` date DEFAULT NULL,
-        \`state\` varchar(50) DEFAULT NULL,
-        \`fips\` varchar(20) DEFAULT NULL,
-        \`cases\` int(11) DEFAULT NULL,
-        \`deaths\` int(11) DEFAULT NULL
-      ) ENGINE=MyISAM DEFAULT CHARSET=utf8;`);
-    await conn.promise().execute('TRUNCATE TABLE `state_cases`;');
-    let count = 0;
-    await Promise.all(stateData.map(item => {
-      count++;
-      return conn.promise().execute('INSERT INTO `state_cases` (date, state, fips, cases, deaths) VALUES (?, ?, ?, ?, ?)', [item.date, item.state, item.fips, item.cases, item.deaths]);
-    }));
-    console.log(`Updated State data, total = ${count}`);
-    await conn.promise().execute(`CREATE TABLE IF NOT EXISTS \`county_cases\` (
-        \`date\` date DEFAULT NULL,
-        \`county\` varchar(50) DEFAULT NULL,
-        \`state\` varchar(50) DEFAULT NULL,
-        \`fips\` varchar(20) DEFAULT NULL,
-        \`cases\` int(11) DEFAULT NULL,
-        \`deaths\` int(11) DEFAULT NULL,
-        KEY \`county_state_index\` (\`county\`,\`state\`)
-      ) ENGINE=MyISAM DEFAULT CHARSET=utf8;`);
-    await conn.promise().execute('TRUNCATE TABLE `county_cases`;');
-    count = 0;
-    await Promise.all(countyData.map(item => {
-      count++;
-      return conn.promise().execute('INSERT INTO `county_cases` (date, county, state, fips, cases, deaths) VALUES (?, ?, ?, ?, ?, ?)', [item.date, item.county, item.state, item.fips, item.cases, item.deaths]);
-    }));
+    await updateStateData(conn);
+    await updateCountyData(conn);
     conn.end();
-    console.log(`Updated county data, total = ${count}`);
   } catch (err) {
     conn.end();
     throw err;
